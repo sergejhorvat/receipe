546cd53e7e6a12a9cd205f6558616190c80357c6
sergejhorvat <sergejhorvat@users.noreply.github.com>
2019-04-19 16:28:52 +0200

    Initial comit

---
+++ src/main/java/com/shorvat/receipe/controllers/IndexController.java
@@ -0,0 +1,4 @@
+package com.shorvat.receipe.controllers;
+
+public class IndexController {
+}
---
+++ src/main/java/com/shorvat/receipe/controllers/ReceipeController.java
@@ -0,0 +1,16 @@
+package com.shorvat.receipe.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ReceipeController {
+
+    private
+
+    public ReceipeController(){
+
+
+
+    }
+
+}
